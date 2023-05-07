3acb102fcfff4442fc44469cf17a2d9dadd79eb2
BUSRA KUCUK <busra3590@gmail.com>
2023-05-07 18:49:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arr){
+    let winGame = arr.find(game => game.result === "W");
+    if(winGame){
+        return winGame.year;
+    } else {
+        return undefined;
+    }
+
+}
